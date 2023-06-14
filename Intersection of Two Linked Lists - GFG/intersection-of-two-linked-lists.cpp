38f109da226714d69df4e22e13e79915de556f6c
Amanrastogi04 <76608731+Amanrastogi04@users.noreply.github.com>
2023-06-14 19:35:09 +0530

    Added solution - LeetHub

---
+++ Intersection of Two Linked Lists - GFG/intersection-of-two-linked-lists.cpp
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+
+// } Driver Code Ends
+/* structure of list node:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+class Solution{
+  public:
+    Node* findIntersection(Node* head1, Node* head2)
+    {
+        unordered_set<int> st;
+        Node* curr=head2;
+        Node* temp=head1;
+        Node* dummy=new Node(INT_MIN);
+        Node* f=dummy;
+        
+        while(curr!=NULL){
+            st.insert(curr->data);
+            curr=curr->next;
+        }
+        
+        while(temp!=NULL){
+            if(st.find(temp->data)!=st.end()){
+                f->next=new Node(temp->data);
+                f=f->next;
+            }
+            temp=temp->next;
+        }
+        
+        return dummy->next;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    
+	    cin>> n;
+	    Node* head1 = inputList(n);
+	    
+	    cin>>m;
+	    Node* head2 = inputList(m);
+	    Solution obj;
+	    Node* result = obj.findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
