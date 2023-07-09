68c92bc335dcd7eb2d440da416e21c3593eb6c69
Amanrastogi04 <76608731+Amanrastogi04@users.noreply.github.com>
2023-07-10 00:56:56 +0530

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.cpp
+++ Geek's Training - GFG/geeks-training.cpp
@@ -58,9 +58,32 @@ class Solution {
     int maximumPoints(vector<vector<int>>& points, int n) {
         // return solve(n-1,3,points);
         
-        vector<vector<int>> dp(n,vector<int>(4,-1));
+        // vector<vector<int>> dp(n,vector<int>(4,-1));
         
-        return solveMem(n-1,3,points,dp);
+        // return solveMem(n-1,3,points,dp);
+        
+        vector<vector<int>> dp(n,vector<int>(4,0));
+        
+        dp[0][0]=max(points[0][1],points[0][2]);
+        dp[0][1]=max(points[0][0],points[0][2]);
+        dp[0][2]=max(points[0][1],points[0][0]);
+        dp[0][3]=max(points[0][0],max(points[0][1],points[0][2]));
+        
+        for(int i=1;i<n;i++){
+            for(int last=0;last<4;last++){
+                dp[i][last]=0;
+                
+                for(int task=0;task<3;task++){
+                    if(last!=task){
+                      int point=points[i][task]+dp[i-1][task];
+                      dp[i][last]=max(point,dp[i][last]);
+                    }
+                }
+            }
+        }
+        
+        return dp[n-1][3];
+        
     }
 };
 
