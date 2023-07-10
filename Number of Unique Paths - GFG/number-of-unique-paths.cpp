63e2f1913e2d7485c693485a90a618696ba05e5a
Amanrastogi04 <76608731+Amanrastogi04@users.noreply.github.com>
2023-07-10 16:59:52 +0530

    Added solution - LeetHub

--- Number of Unique Paths - GFG/number-of-unique-paths.cpp
+++ Number of Unique Paths - GFG/number-of-unique-paths.cpp
@@ -51,8 +51,29 @@ class Solution
     int NumberOfPath(int a, int b)
     {
         // return solve(a-1,b-1);
-        vector<vector<int>> dp(a,vector<int>(b,-1));
-        return solveMem(a-1,b-1,dp);
+        // vector<vector<int>> dp(a,vector<int>(b,-1));
+        // return solveMem(a-1,b-1,dp);
+        
+        vector<vector<int>> dp(a,vector<int>(b,0));
+        
+        for(int i=0;i<a;i++){
+            for(int j=0;j<b;j++){
+                if(i==0 && j==0) dp[i][j]=1;
+                else{
+                    int up=0,left=0;
+                    if(i>0){
+                        up=dp[i-1][j];
+                    }
+                    if(j>0){
+                        left=dp[i][j-1];
+                    }
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        
+        return dp[a-1][b-1];
+        
     }
 };
 
