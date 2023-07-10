6f5c80992daece12101066d2a9e70b2f593ddeb0
Amanrastogi04 <76608731+Amanrastogi04@users.noreply.github.com>
2023-07-10 23:47:42 +0530

    Added solution - LeetHub

--- Triangle Path Sum - GFG/triangle-path-sum.cpp
+++ Triangle Path Sum - GFG/triangle-path-sum.cpp
@@ -41,22 +41,23 @@ class Solution {
         // vector<vector<int>> dp(n,vector<int>(n,-1));
         // return solveMem(0,0,n,triangle,dp);
         
-        vector<vector<int>> dp(n,vector<int>(n,0));
+        vector<int>prev(n,0),curr(n,0);
         
         for(int j=0;j<n;j++){
-            dp[n-1][j]=triangle[n-1][j];
+            prev[j]=triangle[n-1][j];
         }
         
         for(int i=n-2;i>=0;i--){
             for(int j=i;j>=0;j--){
-                int down=triangle[i][j]+dp[i+1][j];
-                int dg=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(down,dg);
+                int down=triangle[i][j]+prev[j];
+                int dg=triangle[i][j]+prev[j+1];
+                curr[j]=min(down,dg);
             }
+            prev=curr;
             
         }
         
-        return dp[0][0];
+        return prev[0];
         
     }
 };
