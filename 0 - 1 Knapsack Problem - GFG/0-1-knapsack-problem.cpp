0b1d92cd6943a94a0add09448ec053f0b2b5b849
Amanrastogi04 <76608731+Amanrastogi04@users.noreply.github.com>
2023-07-12 07:09:44 +0530

    Added solution - LeetHub

---
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    
+    int solve(int index,int w,int wt[],int val[]){
+        if(index==0){
+            if(wt[index]<=w) return val[0];
+            else return 0;
+        }
+        
+        int nottake=solve(index-1,w,wt,val);
+        int take=INT_MIN;
+        if(wt[index]<=w){
+            take=val[index]+solve(index-1,w-wt[index],wt,val);
+        }
+        
+        return max(take,nottake);
+    }
+    
+    
+    int solveMem(int index,int w,int wt[],int val[],vector<vector<int>> &dp){
+        if(index==0){
+            if(wt[index]<=w) return val[0];
+            else return 0;
+        }
+        
+        if(dp[index][w]!=-1){
+            return dp[index][w];
+        }
+        
+        int nottake=solveMem(index-1,w,wt,val,dp);
+        int take=INT_MIN;
+        if(wt[index]<=w){
+            take=val[index]+solveMem(index-1,w-wt[index],wt,val,dp);
+        }
+        
+        return dp[index][w]=max(take,nottake);
+    }
+    
+    
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+        //  return solve(n-1,W,wt,val);
+        
+        vector<vector<int>> dp(n,vector<int>(W+1,-1));
+        return solveMem(n-1,W,wt,val,dp);
+    
+    
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //reading number of elements and weight
+        int n, w;
+        cin>>n>>w;
+        
+        int val[n];
+        int wt[n];
+        
+        //inserting the values
+        for(int i=0;i<n;i++)
+            cin>>val[i];
+        
+        //inserting the weights
+        for(int i=0;i<n;i++)
+            cin>>wt[i];
+        Solution ob;
+        //calling method knapSack()
+        cout<<ob.knapSack(w, wt, val, n)<<endl;
+        
+    }
+	return 0;
+}
+// } Driver Code Ends
