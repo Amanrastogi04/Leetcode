dcf8cee49fe51469295eaae71ed31daf57cdee4d
Amanrastogi04 <76608731+Amanrastogi04@users.noreply.github.com>
2023-08-10 19:56:42 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -10,44 +10,8 @@ class Solution
 {
     public:
     //Function to find the length of longest common subsequence in two strings.
-    
-    int solve(int n,int m,string s1,string s2){
-        
-        if(n<0 || m<0) return 0;
-        
-        if(s1[n]==s2[m]){
-            return 1+solve(n-1,m-1,s1,s2);
-        }
-        
-        return max(solve(n-1,m,s1,s2),solve(n,m-1,s1,s2));
-        
-    }
-    
-    
-    int solveMem(int n,int m,string s1,string s2,vector<vector<int>> &dp){
-        
-        if(n==0 || m==0) return 0;
-        
-        if(dp[n][m]!=-1){
-            return dp[n][m];
-        }
-        
-        if(s1[n-1]==s2[m-1]){
-            return dp[n][m]=1+solveMem(n-1,m-1,s1,s2,dp);
-        }
-        
-        return dp[n][m]=max(solveMem(n-1,m,s1,s2,dp),solveMem(n,m-1,s1,s2,dp));
-        
-    }
-    
-    
     int lcs(int n, int m, string s1, string s2)
     {
-        // return solve(n-1,m-1,s1,s2);
-        
-        // vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        // return solveMem(n,m,s1,s2,dp);
-        
         vector<int> prev(m+1,0),curr(m+1,0);
         for(int i=0;i<=n;i++) prev[0]=0;
 
@@ -65,7 +29,6 @@ class Solution
         }
         
         return prev[m];
-        
     }
 };
 
