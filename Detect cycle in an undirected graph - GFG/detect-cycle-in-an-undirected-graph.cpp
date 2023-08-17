b1db938f2f7cd63c660d2f4a80dbfa0baeae6c2c
Amanrastogi04 <76608731+Amanrastogi04@users.noreply.github.com>
2023-08-17 13:13:21 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -7,32 +7,36 @@ class Solution {
   public:
     // Function to detect cycle in an undirected graph.
     
-    bool dfs(int node,int parent,vector<int> adj[],vector<int> &vis){
-        vis[node]=1;
+    bool bfs(int src,vector<int>adj[],vector<int>&vis){
+        vis[src]=1;
+        queue<pair<int,int>>q;
+        q.push({src,-1});
         
-       
+        while(!q.empty()){
+            int node=q.front().first;
+            int parent=q.front().second;
+            q.pop();
             
-            for(auto it:adj[node]){
-                if(!vis[it]){
-                    if(dfs(it,node,adj,vis)==true)
-                    return true;
-                    
+            for(auto n:adj[node]){
+                if(!vis[n]){
+                    q.push({n,node});
+                    vis[n]=1;
                 }
-                else if(parent!=it){
+                else if(parent!=n){
                     return true;
                 }
-            
+            }
         }
         return false;
+        
     }
     
-    
     bool isCycle(int V, vector<int> adj[]) {
-        
         vector<int> vis(V,0);
+        
         for(int i=0;i<V;i++){
             if(!vis[i]){
-                if(dfs(i,-1,adj,vis)==true){
+                if(bfs(i,adj,vis)){
                     return true;
                 }
             }
